831adddf92501ff803f53cd091b3f314e7275b70
Tfauves <tfauvelle@gmail.com>
2021-11-07 11:11:17 -0500

    initial commit.

---
+++ src/test/java/com/careerdevs/myHalwayLocker/MyHalwayLockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.myHalwayLocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHalwayLockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
