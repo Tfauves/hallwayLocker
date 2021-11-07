cc09bd0c98878c8f9edcbc0263988ab84ea0794b
Tfauves <tfauvelle@gmail.com>
2021-11-07 11:44:07 -0500

    added application properties.

---
+++ src/main/java/com/careerdevs/myHalwayLocker/models/Student.java
@@ -0,0 +1,54 @@
+package com.careerdevs.myHalwayLocker.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id @GeneratedValue private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer cohort;
+
+    public Student() {}
+
+    public Student(String firstName, String lastName, Integer cohort) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cohort = cohort;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getCohort() {
+        return cohort;
+    }
+
+    public void setCohort(Integer cohort) {
+        this.cohort = cohort;
+    }
+}
