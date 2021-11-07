d8c1c52227a370c36c42c99902a3e9897a945c7a
Tfauves <tfauvelle@gmail.com>
2021-11-07 12:30:58 -0500

    get mapping defined.

--- src/main/java/com/careerdevs/myHalwayLocker/controllers/StudentController.java
+++ src/main/java/com/careerdevs/myHalwayLocker/controllers/StudentController.java
@@ -5,10 +5,11 @@ package com.careerdevs.myHalwayLocker.controllers;
 import com.careerdevs.myHalwayLocker.models.Student;
 import com.careerdevs.myHalwayLocker.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,6 +23,16 @@ public class StudentController {
     @GetMapping
     public @ResponseBody List<Student> getAllStudents() {
         return repository.findAll();
+    }
+
+    @GetMapping("/cohort/{cohort}")
+    public ResponseEntity<List<Student>> getByCohort(@PathVariable Integer cohort) {
+        return new ResponseEntity<>(repository.findStudentsByCohort(cohort, Sort.by("name")), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Student findById(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 
--- src/main/java/com/careerdevs/myHalwayLocker/repositories/StudentRepository.java
+++ src/main/java/com/careerdevs/myHalwayLocker/repositories/StudentRepository.java
@@ -1,8 +1,13 @@
 package com.careerdevs.myHalwayLocker.repositories;
 
 import com.careerdevs.myHalwayLocker.models.Student;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    List<Student> findStudentsByCohort(Integer cohort, Sort sort);
 
 }
